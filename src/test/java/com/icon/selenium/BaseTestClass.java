8e0334d0af9eb69d16eb193d14d9525847848200
BrindaVijay <Vijaya.Soman@target.com>
2016-11-02 21:46:20 -0500

    Fix the Jenkins Build

--- src/test/java/com/icon/selenium/BaseTestClass.java
+++ src/test/java/com/icon/selenium/BaseTestClass.java
@@ -5,6 +5,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class BaseTestClass {
 
@@ -12,8 +16,9 @@ public class BaseTestClass {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "/apps/install/jenkins/jobs/Selenium-Auto/geckodriver");
-        webDriver = new FirefoxDriver();
+        //System.setProperty("webdriver.gecko.driver", "/apps/install/jenkins/jobs/Selenium-Auto/geckodriver");
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
     }
 
     public WebDriver getWebDriver() {
