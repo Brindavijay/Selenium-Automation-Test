00943c732db80cbfbccb18f3541f7f8d031ee5da
BrindaVijay <Vijaya.Soman@target.com>
2016-11-02 21:19:20 -0500

    Updated Firefix driver

--- src/test/java/com/icon/selenium/BaseTestClass.java
+++ src/test/java/com/icon/selenium/BaseTestClass.java
@@ -12,6 +12,7 @@ public class BaseTestClass {
 
     @Before
     public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "/apps/install/jenkins/jobs/Selenium-Auto/geckodriver");
         webDriver = new FirefoxDriver();
     }
 
